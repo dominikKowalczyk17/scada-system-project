8348c8431d310715cf032d9185110d6883d3572e
Dominik <dkowalczyk@archlinux.com>
2025-09-18 20:20:13 +0200

    Initial Spring Boot project setup

---
+++ scada-system/src/main/java/com/dkowalczyk/scadasystem/ScadaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dkowalczyk.scadasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScadaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScadaSystemApplication.class, args);
+	}
+
+}
---
+++ scada-system/src/test/java/com/dkowalczyk/scadasystem/ScadaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkowalczyk.scadasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScadaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
