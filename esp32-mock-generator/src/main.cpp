9bba03574c0339a2fcd02bc05070537660fa1a69
Dominik Kowalczyk <kowalczyk.dominik17@gmail.com>
2025-12-26 15:51:24 +0100

    Refactor and add dc offset removal

--- esp32-mock-generator/src/main.cpp
+++ esp32-mock-generator/src/main.cpp
@@ -5,30 +5,25 @@
 #include <arduinoFFT.h>
 #include "config.h"
 
-// --- KONFIGURACJA ---
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASS;
-const char* mqtt_server = MQTT_SERVER;
-const char* mqtt_topic = "scada/measurements/node1";
-
+// --- PARAMETRY POMIAROWE ---
 #define SAMPLES 512
-#define SAMPLING_FREQ 6000 // 6kHz wg timera (166us)
+#define SAMPLING_FREQ 6000 
+#define MAX_CALC_HARMONIC 25
 #define PIN_U 33
 #define PIN_I 35
-#define PIN_REF 32
 
-// Współczynniki kalibracyjne
-float vCoeff = 0.550;     
-float iCoeff = 0.096;
+// --- STAŁE KALIBRACJI (Zweryfikowane) ---
+const float vCoeff = 0.550;     
+const float iCoeff = 0.0096;
+const float NOISE_GATE_RMS = 0.05;
+const float ADC_DEAD_ZONE = 4.0;
 
-// Bufory i flagi
+// --- ZMIENNE SYSTEMOWE ---
 volatile int rawBufferU[SAMPLES];
 volatile int rawBufferI[SAMPLES];
-volatile int rawBufferRef[SAMPLES];
 volatile bool dataReady = false;
 volatile int sampleIdx = 0;
 
-// Tablice do FFT
 double vReal[SAMPLES], vImag[SAMPLES];
 double iReal[SAMPLES], iImag[SAMPLES];
 ArduinoFFT<double> FFT = ArduinoFFT<double>();
@@ -40,9 +35,10 @@ unsigned long lastPublishTime = 0;
 
 void IRAM_ATTR onTimer() {
   if (!dataReady) {
-    rawBufferU[sampleIdx] = analogRead(PIN_U);
-    rawBufferI[sampleIdx] = analogRead(PIN_I);
-    rawBufferRef[sampleIdx] = analogRead(PIN_REF);
+    // Oversampling: 2 odczyty = lepszy SNR
+    rawBufferU[sampleIdx] = (analogRead(PIN_U) + analogRead(PIN_U)) >> 1;
+    rawBufferI[sampleIdx] = (analogRead(PIN_I) + analogRead(PIN_I)) >> 1;
+    
     sampleIdx++;
     if (sampleIdx >= SAMPLES) {
       sampleIdx = 0;
@@ -52,115 +48,114 @@ void IRAM_ATTR onTimer() {
 }
 
 void setup_wifi() {
-    Serial.print("\nConnecting to WiFi");
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("\nWiFi Connected. IP: " + WiFi.localIP().toString());
+    Serial.printf("\nConnecting to %s...", WIFI_SSID);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+    Serial.println("\nWiFi Connected.");
 }
 
 void processingTask(void * pvParameters) {
   for(;;) {
     if (dataReady) {
       unsigned long currentTime = millis();
-      
       if (currentTime - lastPublishTime >= 3000) {
         lastPublishTime = currentTime;
 
-        double sumU2 = 0, sumI2 = 0, pSum = 0;
+        // 1. DC Offset Removal
+        double sumU = 0, sumI = 0;
+        for (int i = 0; i < SAMPLES; i++) { sumU += rawBufferU[i]; sumI += rawBufferI[i]; }
+        float offsetU = sumU / SAMPLES;
+        float offsetI = sumI / SAMPLES;
 
-        // 1. Kopiowanie danych i obliczenia RMS/P
+        // 2. Przygotowanie danych i RMS
+        double sumU2 = 0, sumI2 = 0, sumP = 0;
         for (int i = 0; i < SAMPLES; i++) {
-          float uScaled = (rawBufferU[i] - rawBufferRef[i]) * vCoeff;
-          float iScaled = (rawBufferI[i] - rawBufferRef[i]) * iCoeff;
-          
-          vReal[i] = (double)uScaled;
-          vImag[i] = 0;
-          iReal[i] = (double)iScaled;
-          iImag[i] = 0;
-          
-          sumU2 += uScaled * uScaled;
-          sumI2 += iScaled * iScaled;
-          pSum += uScaled * iScaled;
+            float uScaled = (rawBufferU[i] - offsetU) * vCoeff;
+            float iRaw = (float)rawBufferI[i] - offsetI;
+            
+            if (abs(iRaw) < ADC_DEAD_ZONE) iRaw = 0; 
+            float iScaled = iRaw * iCoeff;
+            
+            vReal[i] = (double)uScaled; vImag[i] = 0;
+            iReal[i] = (double)iScaled; iImag[i] = 0;
+            
+            sumU2 += uScaled * uScaled;
+            sumI2 += iScaled * iScaled;
+            sumP += uScaled * iScaled;
         }
 
         float vRMS = sqrt(sumU2 / SAMPLES);
         float iRMS = sqrt(sumI2 / SAMPLES);
-        float pActive = pSum / SAMPLES;
-        float sApparent = vRMS * iRMS;
-        float qReactive = (sApparent > abs(pActive)) ? sqrt(pow(sApparent, 2) - pow(pActive, 2)) : 0;
-        float cosPhi = (sApparent > 0.1) ? abs(pActive) / sApparent : 1.0;
+        float pActive = sumP / SAMPLES;
 
-        // 2. FFT Napięcia
+        // 3. FFT Napięcia (Częstotliwość i THD_V)
         FFT.windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
         FFT.compute(vReal, vImag, SAMPLES, FFT_FORWARD);
         FFT.complexToMagnitude(vReal, vImag, SAMPLES);
         
         double freq = FFT.majorPeak(vReal, SAMPLES, SAMPLING_FREQ);
         int baseBin = round(freq / (SAMPLING_FREQ / (double)SAMPLES));
-        if (baseBin < 2) baseBin = 4;
+        if (baseBin < 1) baseBin = 1;
 
-        double hV_vals[6];
+        double hV_base = (vReal[baseBin] / SAMPLES) * 2.0;
         double sumSqHarmonicsV = 0;
-        for (int h = 1; h <= 5; h++) {
-            hV_vals[h] = (vReal[baseBin * h] / SAMPLES) * 2.0; 
-            if (h > 1) sumSqHarmonicsV += pow(hV_vals[h], 2);
+        double harmonicsV_out[6]; // Do wysłania w JSON
+
+        for (int h = 1; h <= MAX_CALC_HARMONIC; h++) {
+            int bin = baseBin * h;
+            double amp = (bin < SAMPLES/2) ? (vReal[bin] / SAMPLES) * 2.0 : 0;
+            if (h <= 5) harmonicsV_out[h] = amp;
+            if (h > 1) sumSqHarmonicsV += pow(amp, 2);
         }
 
-        // 3. FFT Prądu
+        // 4. FFT Prądu (THD_I)
         FFT.windowing(iReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
         FFT.compute(iReal, iImag, SAMPLES, FFT_FORWARD);
         FFT.complexToMagnitude(iReal, iImag, SAMPLES);
 
-        double hI_vals[6];
+        double hI_base = (iReal[baseBin] / SAMPLES) * 2.0;
         double sumSqHarmonicsI = 0;
-        for (int h = 1; h <= 5; h++) {
-            hI_vals[h] = (iReal[baseBin * h] / SAMPLES) * 2.0;
-            if (h > 1) sumSqHarmonicsI += pow(hI_vals[h], 2);
+        double harmonicsI_out[6];
+
+        for (int h = 1; h <= MAX_CALC_HARMONIC; h++) {
+            int bin = baseBin * h;
+            double amp = (bin < SAMPLES/2) ? (iReal[bin] / SAMPLES) * 2.0 : 0;
+            if (h <= 5) harmonicsI_out[h] = amp;
+            if (h > 1) sumSqHarmonicsI += pow(amp, 2);
         }
 
-        // --- DODANY WARUNEK: NOISE GATE ---
-        if (iRMS < 0.40) { 
-          iRMS = 0;
-          pActive = 0;
-          sApparent = 0;
-          qReactive = 0;
-          cosPhi = 1.0;
-          for (int h = 1; h <= 5; h++) {
-            hI_vals[h] = 0;
-          }
-          sumSqHarmonicsI = 0;
+        // 5. Obliczanie parametrów mocy i Noise Gate
+        float sApparent = vRMS * iRMS;
+        float qReactive = (sApparent > abs(pActive)) ? sqrt(pow(sApparent, 2) - pow(pActive, 2)) : 0;
+        float cosPhi = (sApparent > 0.1) ? abs(pActive) / sApparent : 1.0;
+
+        if (iRMS < NOISE_GATE_RMS) {
+            iRMS = 0; pActive = 0; sApparent = 0; qReactive = 0; cosPhi = 1.0;
+            sumSqHarmonicsI = 0; hI_base = 0;
+            for(int i=1; i<=5; i++) harmonicsI_out[i] = 0;
         }
 
-        // 4. Budowanie JSON (ArduinoJson v7)
-        JsonDocument doc;
-        doc["voltage_rms"] = vRMS;
-        doc["current_rms"] = iRMS;
-        doc["power_active"] = abs(pActive);
-        doc["power_apparent"] = sApparent;
-        doc["power_reactive"] = qReactive;
-        doc["cos_phi"] = (cosPhi > 1.0) ? 1.0 : cosPhi;
-        doc["frequency"] = (freq > 40 && freq < 60) ? freq : 50.0;
+        // 6. Budowanie JSON (Oszczędność miejsca)
+        StaticJsonDocument<1024> doc;
+        doc["v_rms"] = round(vRMS * 10) / 10.0;
+        doc["i_rms"] = round(iRMS * 1000) / 1000.0;
+        doc["p_act"] = abs(round(pActive * 10) / 10.0);
+        doc["cos_phi"] = round(cosPhi * 100) / 100.0;
+        doc["freq"] = (freq > 45 && freq < 55) ? round(freq * 10) / 10.0 : 50.0;
         
-        doc["thd_voltage"] = (hV_vals[1] > 5.0) ? (sqrt(sumSqHarmonicsV) / hV_vals[1]) * 100.0 : 0;
-        doc["thd_current"] = (hI_vals[1] > 0.1) ? (sqrt(sumSqHarmonicsI) / hI_vals[1]) * 100.0 : 0;
+        doc["thd_v"] = (hV_base > 10) ? (sqrt(sumSqHarmonicsV) / hV_base) * 100.0 : 0;
+        doc["thd_i"] = (hI_base > 0.01) ? (sqrt(sumSqHarmonicsI) / hI_base) * 100.0 : 0;
 
-        JsonArray hV = doc["harmonics_v"].to<JsonArray>();
-        JsonArray hI = doc["harmonics_i"].to<JsonArray>();
+        JsonArray hV_arr = doc.createNestedArray("harm_v");
+        JsonArray hI_arr = doc.createNestedArray("harm_i");
         for (int h = 1; h <= 5; h++) {
-            hV.add(hV_vals[h]);
-            hI.add(hI_vals[h]);
+            hV_arr.add(round(harmonicsV_out[h] * 100) / 100.0);
+            hI_arr.add(round(harmonicsI_out[h] * 1000) / 1000.0);
         }
 
         char buffer[1024];
         serializeJson(doc, buffer);
-
-        if (client.connected()) {
-          client.publish(mqtt_topic, buffer);
-          Serial.println("[MQTT] Data sent successfully");
-        }
+        if (client.connected()) client.publish(MQTT_TOP, buffer);
         Serial.println(buffer);
       }
       dataReady = false; 
@@ -173,29 +168,22 @@ void processingTask(void * pvParameters) {
 void setup() {
   Serial.begin(115200);
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-
+  client.setServer(MQTT_SERVER, 1883);
   analogReadResolution(12);
   analogSetAttenuation(ADC_11db);
 
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 166, true); // 6024 Hz approx
+  timerAlarmWrite(timer, 166, true);
   timerAlarmEnable(timer);
 
-  xTaskCreatePinnedToCore(processingTask, "Proc", 15000, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(processingTask, "Proc", 10000, NULL, 1, NULL, 1);
 }
 
 void loop() {
   if (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("ESP32_SCADA_Node1")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      delay(5000);
-    }
+    if (client.connect("ESP32_SCADA_Node1")) Serial.println("MQTT Connected");
+    else delay(5000);
   }
   delay(1000);
 }
