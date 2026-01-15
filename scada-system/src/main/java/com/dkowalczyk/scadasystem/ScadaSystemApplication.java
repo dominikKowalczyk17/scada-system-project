e4e9fbac2ea14059f863e27dfe15eb952aecd500
Dominik Kowalczyk <dominik.kowalczyk17@gmail.com>
2026-01-15 20:12:29 +0100

    Fix syntax error in ScadaSystemApplication.java - remove stray '+' character

--- scada-system/src/main/java/com/dkowalczyk/scadasystem/ScadaSystemApplication.java
+++ scada-system/src/main/java/com/dkowalczyk/scadasystem/ScadaSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ScadaSystemApplication {+
+public class ScadaSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScadaSystemApplication.class, args);
